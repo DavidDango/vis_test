0f5e2fa96202e7854d3aefd4dbe19a4340cd6414
DavidDango <David.ra.157@gmail.com>
2019-05-02 03:26:22 -0400

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,88 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <button type="button" @click="toggle_physics"> Turn off physics </button>
+    <div id="outer">
+      <div id="mynetwork"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import vis from 'vis'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      nodes: [
+        { id: 0, label: '0', x: -147, y: -77 },
+        { id: 1, label: '1', x: -186, y: 88 },
+        { id: 2, label: '2', x: 8, y: 160 },
+        { id: 3, label: '3', x: 159, y: 28 },
+        { id: 4, label: '4', x: 45, y: -111 }
+      ],
+      edges: [
+        { from: 0, to: 1 },
+        { from: 0, to: 1 },
+        { from: 0, to: 2 },
+        { from: 0, to: 3 },
+        { from: 0, to: 4 },
+        { from: 0, to: 4 },
+        { from: 1, to: 2 },
+        { from: 1, to: 3 },
+        { from: 1, to: 3 },
+        { from: 2, to: 3 },
+        { from: 2, to: 4 },
+        { from: 3, to: 4 }
+      ],
+      options: {
+        manipulation: {
+          enabled: true,
+          initiallyActive: true,
+          addEdge: function (edgeData, callback) {
+            callback(edgeData)
+          }
+        },
+        nodes: {}
+      },
+      container: '',
+      network: null
+    }
+  },
+  computed: {
+    graph_data () {
+      return {
+        nodes: this.nodes,
+        edges: this.edges
+      }
+    }
+  },
+  mounted () {
+    this.container = document.getElementById('mynetwork')
+    this.network = new vis.Network(this.container, this.graph_data, this.options)
+  },
+  methods: {
+    toggle_physics () {
+      this.options.nodes.physics = false
+      this.network.setOptions(this.options)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  #mynetwork {
+    width: 600px;
+    height: 400px;
+    border: 1px solid lightgray;
+    display: inline-block;
+  }
+  #outer {
+    width: 100%;
+    text-align: center;
+  }
 </style>
